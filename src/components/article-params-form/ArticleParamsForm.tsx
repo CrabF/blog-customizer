3e1a488aef0c77c05ef64db6d4f3349146db787c
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-07-02 11:12:41 +0300

    add animation for opening form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,26 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
+
+
+export type OnClick = () => void;
 
 export const ArticleParamsForm = () => {
+
+  const [isFormOpen, setFromOpened] = useState(false)
+
+  function toggleOpenedForm() {
+    setFromOpened(!isFormOpen);
+  }
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton 
+        OnClick = {()=>{toggleOpenedForm()}}
+        isFormOpen = {isFormOpen}/>
+			<aside className={clsx(styles.container, isFormOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
